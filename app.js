91ce340223b1d2cf9447b2a29d9183d6c833b74e
Yuri.F <yria1@pop-os.localdomain>
2022-03-06 20:24:28 -0300

    important em algumas classes.

--- app.js
+++ app.js
@@ -5,20 +5,5 @@ document.querySelector("#mybtn").addEventListener("click", (e) => {
   splash.classList.add("display-none");
 });
 document.querySelector("#mybtn").onclick = function () {
-  console.log("works!");
-  //$(".plus").click(function(){
-    //$(this).toggleClass("minus")  ; 
-   //})  
   setTimeout("location.href = 'mainwc.html';", 6500);
 };
-
-
-//$( element ).click(function() {
-//$( "#sumir" ).fadeOut( "slow", function() {
-//splash.classList.add("display-none");
-// Animation complete.
-// });
-//});
-//document.getElementById("mybtn").onclick = function () {
-//location.href = "dedEtecloading.gif";
-//};
